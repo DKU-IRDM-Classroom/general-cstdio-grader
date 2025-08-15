0e81e480e7feede7dfb5674eac6ec49e72fed1ae
theeluwin <theeluwin@gmail.com>
2025-08-15 16:04:21 +0900

    hm

--- compare.py
+++ compare.py
@@ -43,15 +43,18 @@ def main():
     out_lines = get_lines_from_file(out_path)
 
     if len(ans_lines) != len(out_lines):
+        print("[DEBUG] num lines mismatch", len(ans_lines), len(out_lines))
         sys.exit(2)
 
     for ans_line, out_line in zip(ans_lines, out_lines):
 
         if len(ans_line) != len(out_line):
+            print("[DEBUG] num tokens mismatch", ans_line, out_line)
             sys.exit(2)
 
         for ans_token, out_token in zip(ans_line, out_line):
             if ans_token != out_token:
+                print("[DEBUG] token mismatch", ans_token, out_token)
                 sys.exit(1)
 
     sys.exit(0)
