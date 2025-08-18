3a0254eab238c583818e5c901c6c5dd60838f18c
theeluwin <theeluwin@gmail.com>
2025-08-19 03:08:16 +0900

    hm

--- generate_result.py
+++ generate_result.py
@@ -1,30 +1,37 @@
 import sys
 import json
 
+from pathlib import Path
+
 
 def generate_result(total_score: int, pass_score: int):
     return {
-        'version': 1,
-        'status': "pass",
+        # 'version': 1,
+        'status': 'pass',
         'max_score': total_score,
         'tests': [
             {
-                'name': 'Test',
+                # 'name': 'Test',
                 'status': 'pass' if pass_score > 0 else 'fail',
                 'score': pass_score,
-                'message': 'some_message_here',
-                'test_code': 'some_command_heere',
-                'filename': 'some_filename_here',
-                'line_no': 0,
-                'duration': 0,
+                # 'message': 'some_message_here',
+                # 'test_code': 'some_command_heere',
+                # 'filename': 'some_filename_here',
+                # 'line_no': 0,
+                # 'duration': 0,
             },
         ],
     }
 
 
 if __name__ == '__main__':
-    total_score = int(sys.argv[1])
-    pass_score = int(sys.argv[2])
+
+    base_dir = Path(sys.argv[1]).absolute()
+
+    total_score = int(sys.argv[2])
+    pass_score = int(sys.argv[3])
+
     result = generate_result(total_score, pass_score)
-    with open('results.json', 'w') as file_pointer:
+
+    with open(base_dir / 'results.json', 'w') as file_pointer:
         json.dump(result, file_pointer, indent=2)
